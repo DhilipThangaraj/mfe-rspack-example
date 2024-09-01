59eac885c42ab0994f450b466a685a9f8fe6152c
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2024-09-01 13:38:58 +0400

    follow up commit with scafolding added tailwind config shadcn config tsconfig etc..

--- packages/payment/src/App.tsx
+++ packages/payment/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
+import RegistrationForm from "./components/Forms/RegistrationForm/RegistrationForm";
 
-const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
-};
+import "./App.css";
+
+RegistrationForm;
+
+function App() {
+  return <RegistrationForm />;
+}
 
 export default App;
---
+++ packages/payment/src/components/Forms/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,5 @@
+const RegistrationForm = () => {
+  return <div>{"Registration Form"}</div>;
+};
+
+export default RegistrationForm;
